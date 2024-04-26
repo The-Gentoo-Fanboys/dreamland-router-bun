85d65ec1cd94a62c824294a18f8fb8b14f0c21c9
endercass <46408156+Endercass@users.noreply.github.com>
2024-04-26 17:20:16 -0400

    add class to active links

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,62 @@
 let $activerouter: ThisParameterType<typeof Route> | null;
 let $state: string[] = [];
+let $links: DLElement<{
+  class?: string;
+  activeClass?: string;
+  active?: boolean;
+  href: string;
+}>[] = [];
 
-export const Route: Component<{
-  path: string
-  show?: ComponentElement<any>
-  regex?: boolean
-  render: (root: HTMLElement) => void
-  route: (path: string) => void
-}, {
-  _route: (path: string) => void
-  children: ComponentElement<typeof Route>[] | ComponentElement<typeof Redirect>[]
-}, "route" | "render"> = function() {
+export const Route: Component<
+  {
+    path: string;
+    show?: ComponentElement<any>;
+    regex?: boolean;
+    render: (root: HTMLElement) => void;
+    route: (path: string) => void;
+  },
+  {
+    _route: (path: string) => void;
+    children: // @ts-ignore - self-referential type
+    ComponentElement<typeof Route>[] | ComponentElement<typeof Redirect>[];
+  },
+  "route" | "render"
+> = function () {
   return <div />;
-}
+};
 
-Route.prototype.render = function(this: ThisParameterType<typeof Route>, root: HTMLElement) {
+Route.prototype.render = function (
+  this: ThisParameterType<typeof Route>,
+  root: HTMLElement,
+) {
   $activerouter = this;
   root.appendChild(this.root);
   this.route(location.pathname);
-  window.addEventListener('popstate', () => {
+  window.addEventListener("popstate", () => {
     this.route($state.pop()!);
   });
-}
+};
 
-Route.prototype.route = function(this: ThisParameterType<typeof Route>, path: string) {
-  let a = <a href={path}></a> as HTMLAnchorElement;
+Route.prototype.route = function (
+  this: ThisParameterType<typeof Route>,
+  path: string,
+) {
+  for (const link of $links) {
+    link.$.active = link.$.href == path;
+  }
+  let a = (<a href={path}></a>) as HTMLAnchorElement;
   let pathname = new URL(a.href).pathname;
   $state.push(pathname);
   history.pushState({}, "", pathname);
-  if (pathname[pathname.length - 1] != "/")
-    pathname += "/"
+  if (pathname[pathname.length - 1] != "/") pathname += "/";
   this._route(pathname.replace(this.path, ""));
-}
-Route.prototype._route = function(this: ThisParameterType<typeof Route>, path: string) {
+};
+Route.prototype._route = function (
+  this: ThisParameterType<typeof Route>,
+  path: string,
+) {
   if (this.children.length < 1) return;
-  if (this.show?.$)
-    this.show.$.outlet = "";
+  if (this.show?.$) this.show.$.outlet = "";
   a: for (const { $: route } of this.children) {
     let paths = path.split("/");
     let route_paths = route.path.split("/");
@@ -49,20 +70,18 @@ Route.prototype._route = function(this: ThisParameterType<typeof Route>, path: s
       if (route.regex) {
         let regex = new RegExp(routepath);
         if (!regex.test(path)) {
-          continue a
+          continue a;
         }
       } else {
         if (routepath.startsWith(":")) {
           let varname = routepath.slice(1);
-          if (route.show)
-            route.show.$[varname] = path;
+          if (route.show) route.show.$[varname] = path;
         } else if (routepath != path) {
-          continue a
+          continue a;
         }
       }
     }
     // console.log("matched with", route.path, route_paths, paths);
-
 
     if (route instanceof Redirect) {
       // console.log("redirecting to", route.to);
@@ -77,32 +96,59 @@ Route.prototype._route = function(this: ThisParameterType<typeof Route>, path: s
         this.root = comp;
       }
 
-      route['_route'](paths.join("/"));
+      route["_route"](paths.join("/"));
     }
     return;
   }
 
-  if (this.show?.$)
-    this.show.$.outlet = "";
-}
+  if (this.show?.$) this.show.$.outlet = "";
+};
 
-export const Redirect: Component<{
-  path: string
-  to: string
-}, {}> = function() {
+export const Redirect: Component<
+  {
+    path: string;
+    to: string;
+  },
+  {}
+> = function () {
   return <div />;
-}
+};
 
-export const Link: Component<{
-  class?: any
-  href: string
-}, {
-  children: any
-}> = function() {
-  this.class ??= "";
-  return <a href={this.href} on:click={(e) => {
-    e.preventDefault();
-    e.stopImmediatePropagation();
-    $activerouter?.route(this.href)
-  }} class={this.class}>{this.children}</a>;
-}
+export const Link: Component<
+  {
+    class?: string;
+    activeClass?: string;
+    active?: boolean;
+    href: string;
+  },
+  {
+    children: any;
+  }
+> = function () {
+  this.class ??= "dllink";
+  this.activeClass ??= "active";
+  this.active ??= false;
+
+  const el = (
+    <a
+      href={this.href}
+      on:click={(e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        $activerouter?.route(this.href);
+      }}
+      class={[
+        use(this.class),
+        use(this.active, (active) => active && this.activeClass),
+      ]}
+    >
+      {this.children}
+    </a>
+  );
+
+  this.mount = () => {
+    $links.push(el);
+  };
+
+  return el;
+};
